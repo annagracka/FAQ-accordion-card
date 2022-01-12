b5f183e105906c64fd3e6a2cd5a59b7bece6e994
zigzagwanderer <an.noisens@gmail.com>
2022-01-12 12:33:39 +0100

    feat: finish show and hide answer

--- src/app.js
+++ src/app.js
@@ -1,25 +1,47 @@
 const elem = document.getElementById('art')
 const arrow = elem.getElementsByTagName('path');
 const question = document.querySelectorAll('article');
-const answer = document.getElementsByClassName('.answer')
-
-
 
 question.forEach(element => {
-    element.addEventListener('click', showText)
-})
+    element.addEventListener('click', showAnswer);
+});
 
+function showAnswer() {
 
-function showText() {
     const elem = this.childNodes.length;
     const message = this.childNodes[elem - 2];
 
-    this.style.color = '#000';
-    this.style.fontWeight = 700;
-    this.style.paddingBottom = 0;
+    if (!this.classList.contains('active')) {
+        hideAnswer();
 
-    message.style.overflow = 'visible';
-    message.style.maxHeight = '100%';
+        this.classList.add('active');
+
+        this.style.fontWeight = 700;
+        this.style.paddingBottom = 0;
+
+        message.style.overflow = 'visible';
+        message.style.maxHeight = '100%';
+
+    }
+    else {
+        hideAnswer();
+    };
+}
 
 
+function hideAnswer() {
+    const activeAnswers = document.querySelectorAll('.active')
+
+    activeAnswers.forEach(element => {
+        element.classList.remove('active');
+
+        const elem = element.childNodes.length;
+        const message = element.childNodes[elem - 2];
+
+        element.style.fontWeight = 500;
+        element.style.paddingBottom = '18px';
+
+        message.style.overflow = 'hidden';
+        message.style.maxHeight = '0';
+    });
 }
