475cda45f90620a12e53729280e5c336047685fd
zigzagwanderer <an.noisens@gmail.com>
2022-02-28 21:01:00 +0100

    feat: add event listener, doesnt cover all div, if click on h2 element, doesnt expand accordion; style: arrow changes its position during rotation

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
-const faq = document.querySelectorAll('.faq__item')
+const section = document.querySelector('section');
 
-faq.forEach(question => {
-    question.addEventListener('click', function() {
-        this.classList.toggle('question__with-answer');
-    })
-})
+
+section.addEventListener("click", function(e) {
+
+
+  const questions = Array.from(document.querySelectorAll(".faq__item"));
+  
+
+  for (let i = 0; i < questions.length; i++) {
+    var question = questions[i]
+    question.classList.remove('question--with-answer');
+  }
+
+  const currentQuestion = e.target;
+  currentQuestion.classList.toggle('question--with-answer')
+});
